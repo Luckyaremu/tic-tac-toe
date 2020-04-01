ad59af6d9febbace6983a998ce0adce902f94170
Luckyaremu <aremu.baba70@gmail.com>
2020-04-01 10:44:46 +0100

    players updated

--- bin/main.rb
+++ bin/main.rb
@@ -16,12 +16,27 @@ def game_start
     print 'player two name:'
     player_two_name = gets.chomp
     break unless player_two_name.empty?
+
     puts 'Kindly enter a name here for player two'
   end
 
-  player_one = Player.new(player_one_name, '0')
-  player_two = Player.new(player_two_name, 'x')
+  player_one = Player.new(player_one, '0')
+  player_two = Player.new(player_two, 'x')
   game = Game.new(player_one, player_two)
-  game.time
+  #game.turn
+
+  puts "game just started?"
 end
+
+def initialize(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
+    @winarray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+    @turn_count = 0
+    @field = ['',
+              '1', '2', '3',
+              '4', '5', '6',
+              '7', '8', '9']
+  end
+
 game_start
